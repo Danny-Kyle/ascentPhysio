ef32cb0e1c3abd3f2af50ed4d06983bc3af7aaa7
Okechukwu Daniel <dikedaniel7917@gmail.com>
2024-11-29 11:46:05 +0100

    huge changes

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const section = document.querySelector(this.getAttribute('href'));
-        if(section) {
-            section.scrollIntoView({
-                behavior: 'smooth'
-            });
+async function loadComponent(file, target) {
+    try {
+        const response = await fetch(file);
+        const content = await response.text();
+        document.querySelector(target).innerHTML = content;
+    } catch (error) {
+        console.error(`Error loading ${file}:`, error);
+    }
+}
+
+function highlightActiveLink(){
+    const currentPage = window.location.pathname.split("/").pop();
+    const navlinks = document.querySelectorAll(".nav-btn");
+
+    navlinks.forEach((link) => {
+        link.classList.remove("active");
+        if(link.getAttribute("href") === currentPage){
+            link.classList.add("active")
         }
     })
+}
+
+document.addEventListener("DOMContentLoaded", async() => {
+    await loadComponent("components/header.html", "#header-placeholder");
+    await loadComponent("components/footer.html", "#footer-placeholder");
+
+    const contentMap = {
+        "index.html": "main/home-content.html",
+        "News.html": "main/news-content.html",
+        "OurTeam.html": "main/team-content.html",
+        "P&S.html": "main/ps-content.html",
+        "Facilities.html": "main/facilities-content.html",
+        "Rates.html": "main/rates-content.html",
+        "weblinks.html": "main/weblinks-content.html",
+        "Contact.html": "main/contact-content.html",
+    };
+
+    const currentPage = window.location.pathname.split("/").pop();
+    const contentFile = contentMap[currentPage]  || "main/homecontent.html"
+    await loadComponent(contentFile, "#main-content");
+
+    highlightActiveLink()
 })
